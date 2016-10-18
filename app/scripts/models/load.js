547c60a873d65069c9344a79c1699c6b78fece02
Colin McKeon <14mckeon@gmail.com>
2016-10-18 13:22:43 -0400

    initial commit

---
+++ app/scripts/models/load.js
@@ -0,0 +1,13 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+
+
+var LoadBtn = Backbone.Model.extend({
+
+});
+
+
+var LoadBtnCollection = Backbone.Collection.exten({
+  
+
+});
