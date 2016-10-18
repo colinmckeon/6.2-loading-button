081ebf01f294a171cc706d6b318a862f95536922
Colin McKeon <14mckeon@gmail.com>
2016-10-18 13:48:10 -0400

    recieving data from the server

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,19 @@
+
+var Backbone = require('backbone');
 var $ = require('jquery');
+
+
+var LoadBtn = Backbone.Model.extend();
+
+
+var LoadBtnCollection = Backbone.Collection.extend({
+    model: LoadBtn,
+    url: 'http://tiny-lasagna-server.herokuapp.com/collections/posts'
+
+
+});
+
+var load = new LoadBtnCollection();
+load.fetch().then(function(){
+  console.log(load);
+})
--- app/scripts/models/load.js
+++ app/scripts/models/load.js
@@ -1,13 +1,1 @@
-var Backbone = require('backbone');
-var $ = require('jquery');
 
-
-var LoadBtn = Backbone.Model.extend({
-
-});
-
-
-var LoadBtnCollection = Backbone.Collection.exten({
-  
-
-});
